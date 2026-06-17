a15bd4afbbfbcf9a062246b3b68cdb29944348d1
Prince Sharma <princesharma17@gmail.com>
2026-06-17 17:08:31 +0530

    Add data insertion examples for student table

--- 01-basics/insert_data.sql
+++ 01-basics/insert_data.sql
@@ -1,16 +1,25 @@
--- Use database
+-- ==================================================
+-- File: insert_data.sql
+-- Topic: Insert Records
+-- Purpose: Learn how to add data into tables
+-- ==================================================
+
+-- Select database
 USE CollegeDB;
 
--- Insert records
+-- Insert first student
 INSERT INTO Student
 VALUES (1,'Prince',19,'Delhi');
 
+-- Insert second student
 INSERT INTO Student
 VALUES (2,'Rahul',20,'Noida');
 
+-- Insert third student
 INSERT INTO Student
 VALUES (3,'Aman',18,'Ghaziabad');
 
+-- Insert fourth student
 INSERT INTO Student
 VALUES (4,'Karan',21,'Meerut');
 
