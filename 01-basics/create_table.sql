cf49ef2b5230dcd1e3e0a5bb97c841f99269e384
Prince Sharma <princesharma17@gmail.com>
2026-06-17 16:07:44 +0530

    Add table creation example

---
+++ 01-basics/create_table.sql
@@ -0,0 +1,12 @@
+-- Use the database
+
+USE CollegeDB;
+
+-- Create Student table
+
+CREATE TABLE Student (
+    Student_ID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Age INT,
+    City VARCHAR(50)
+);
