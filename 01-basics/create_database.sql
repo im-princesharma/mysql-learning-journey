f3f575205df8c4a4e50b8fcd77430ac9c7c2722a
Prince Sharma <princesharma17@gmail.com>
2026-06-17 16:57:08 +0530

    Add database creation and management examples

--- 01-basics/create_database.sql
+++ 01-basics/create_database.sql
@@ -1,4 +1,10 @@
--- Create a database
+-- ==================================================
+-- File: create_database.sql
+-- Topic: Database Creation
+-- Purpose: Learn how to create and use databases
+-- ==================================================
+
+-- Create a new database
 CREATE DATABASE CollegeDB;
 
 -- Display all databases
@@ -7,12 +13,11 @@ SHOW DATABASES;
 -- Select the database
 USE CollegeDB;
 
--- Check current database
+-- Display current database
 SELECT DATABASE();
 
 -- Create another sample database
 CREATE DATABASE StudentDB;
 
--- Show databases again
+-- Display databases again
 SHOW DATABASES;
-
