93227c50766723b6af894c23a38f91364eca96bf
Prince Sharma <princesharma17@gmail.com>
2026-06-17 16:08:11 +0530

    Add record insertion examples

---
+++ 01-basics/insert_data.sql
@@ -0,0 +1,10 @@
+-- Insert records into Student table
+
+INSERT INTO Student
+VALUES (1, 'Prince', 19, 'Delhi');
+
+INSERT INTO Student
+VALUES (2, 'Rahul', 20, 'Noida');
+
+INSERT INTO Student
+VALUES (3, 'Aman', 18, 'Ghaziabad');
