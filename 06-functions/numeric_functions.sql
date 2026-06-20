5b1e2d1e4da8d97fd59a55b7c234a0b49adf8c8c
Prince Sharma <iprincesharma17@gmail.com>
2026-06-21 00:29:58 +0530

    Add numeric function examples for mathematical calculations

---
+++ 06-functions/numeric_functions.sql
@@ -0,0 +1,19 @@
+-- File: numeric_functions.sql
+-- Topic: Numeric Functions
+-- Purpose: Perform mathematical operations
+
+SELECT ABS(-50) AS AbsoluteValue;
+
+SELECT ROUND(45.678, 2) AS RoundedValue;
+
+SELECT CEIL(45.2) AS CeilingValue;
+
+SELECT FLOOR(45.9) AS FloorValue;
+
+SELECT MOD(17, 5) AS Remainder;
+
+SELECT POWER(2, 5) AS PowerValue;
+
+SELECT SQRT(64) AS SquareRoot;
+
+SELECT RAND() AS RandomNumber;
