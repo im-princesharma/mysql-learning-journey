00bb6251c801bfd63fd963d71b844be9fe127380
Prince Sharma <iprincesharma17@gmail.com>
2026-06-21 00:22:14 +0530

    Add arithmetic operator examples with practical calculation scenarios

---
+++ 05-operators/arithmetic_operators.sql
@@ -0,0 +1,26 @@
+-- File: arithmetic_operators.sql
+-- Topic: Arithmetic Operators
+-- Purpose: Perform mathematical calculations
+
+SELECT 10 + 5 AS Addition;
+SELECT 20 - 8 AS Subtraction;
+SELECT 6 * 7 AS Multiplication;
+SELECT 50 / 5 AS Division;
+SELECT 17 % 5 AS Modulus;
+
+-- Student marks example
+SELECT
+    80 + 15 AS TotalMarks;
+
+SELECT
+    50000 + 5000 AS SalaryIncrement;
+
+SELECT
+    1000 - 250 AS RemainingBalance;
+
+SELECT
+    12 * 12 AS SquareArea;
+
+SELECT
+    100 / 4 AS AverageValue;
+    
